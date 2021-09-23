89f2a372faea4d1191032f958e049b55537dacfa
William Li <email@williamli.dev>
2021-09-23 14:02:12 +0800

    added project b to d

--- home/next.config.js
+++ home/next.config.js
@@ -13,6 +13,30 @@ module.exports = {
         source: '/blog/:path*',
         destination: `https://40049-nextjs-with-zones-blog.vercel-support.app/blog/:path*`,
       },
+      {
+        source: '/b',
+        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/blog`,
+      },
+      {
+        source: '/b/:path*',
+        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/blog/:path*`,
+      },
+      {
+        source: '/c',
+        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/blog`,
+      },
+      {
+        source: '/c/:path*',
+        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/blog/:path*`,
+      },
+      {
+        source: '/d',
+        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/blog`,
+      },
+      {
+        source: '/d/:path*',
+        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/blog/:path*`,
+      },
     ]
   },
 }
