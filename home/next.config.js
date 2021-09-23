63f9ce044782b4aca2e31ddf1947b52a0c3c86fb
William Li <email@williamli.dev>
2021-09-23 14:07:38 +0800

    fix rewrites

--- home/next.config.js
+++ home/next.config.js
@@ -15,27 +15,27 @@ module.exports = {
       },
       {
         source: '/b',
-        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/blog`,
+        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/b`,
       },
       {
         source: '/b/:path*',
-        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/blog/:path*`,
+        destination: `https://40049-nextjs-with-zones-b.vercel-support.app/b/:path*`,
       },
       {
         source: '/c',
-        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/blog`,
+        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/c`,
       },
       {
         source: '/c/:path*',
-        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/blog/:path*`,
+        destination: `https://40049-nextjs-with-zones-c.vercel-support.app/c/:path*`,
       },
       {
         source: '/d',
-        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/blog`,
+        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/d`,
       },
       {
         source: '/d/:path*',
-        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/blog/:path*`,
+        destination: `https://40049-nextjs-with-zones-d.vercel-support.app/d/:path*`,
       },
     ]
   },
