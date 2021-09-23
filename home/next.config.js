e7bf775e33cae02af40cfc7c230d625ba0b1665f
William Li <email@williamli.dev>
2021-09-23 13:47:39 +0800

    testing out next.js with zone support

---
+++ home/next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/blog',
+        destination: `https://40049-nextjs-with-zones-blog.vercel-support.app/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `https://40049-nextjs-with-zones-blog.vercel-support.app/blog/:path*`,
+      },
+    ]
+  },
+}
